ff8f91844dbbba9fc891886920d41ea64de0ff25
Jeremy Puchta <jeremypuchta@outlook.com>
2020-09-18 18:13:38 +0200

    Write new introduction

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,11 @@ export default function IndexPage({ posts }) {
       <section className="mb-8">
         <h1 className="text-4xl font-bold">Hey, I'm Jerry! 🐊</h1>
         <br />
-        <p className="text-xl">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+        <p className="text-xl text-justify">
+          I'm a software engineer in love with learning new things.
+          On this website I share my latest work and thoughts about various subjects.
+          Reach out to me on <a href="https://twitter.com/jeremypuchta">Twitter</a> or meet me in person in <a href="https://www.google.com/maps/place/Leipzig/">Leipzig</a> for a coffee or beer.
+        </p>
       </section>
       <section>
         <h2 className="text-2xl font-bold">Articles</h2>
