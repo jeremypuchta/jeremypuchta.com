81ea4ca5c681d8ff5c6a12b431e1bcb9344c9f9e
Jeremy Puchta <jeremypuchta@outlook.com>
2020-10-05 21:57:27 +0200

    Remove post reference from Post component

--- pages/[id].js
+++ pages/[id].js
@@ -1,18 +1,10 @@
 import Layout from "../components/layout";
-import ReactMarkdown from "react-markdown";
-import moment from "moment";
 
 export default function Post() {
   return (
     <Layout>
       <article>
-        <div className="pb-4">
-          <h2 className="text-4xl sm:text-6xl font-extrabold">{post.title}</h2>
-          <p className="italic">Published on {moment(post.date).format('MMMM DD, YYYY')}</p>
-        </div>
-        <div className="markdown">
-          <ReactMarkdown source={post.content} />
-        </div>
+        <p>This is a post</p>
       </article>
     </Layout>
   )
