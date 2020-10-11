1e788299ea3de5d37f73c33c2781b82ad20e3b2e
Jeremy Puchta <jeremypuchta@outlook.com>
2020-10-11 13:00:25 +0200

    Add Google Analytics

--- components/layout.js
+++ components/layout.js
@@ -7,6 +7,20 @@ function Layout({ children }) {
     <div className="container max-w-screen-md mx-auto pt-12 px-8">
       <Head>
         <meta name="og:title" content="Jeremy Puchta Personal Website" />
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}/>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}>
+        </script>
       </Head>
       <Header />
       <main className="min-h-screen">{children}</main>
