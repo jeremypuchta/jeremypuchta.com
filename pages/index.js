7db5ec6fe309e80dfcdf075b6df89b034fe6b146
Jeremy Puchta <jeremypuchta@outlook.com>
2021-01-27 10:05:32 +0100

    Slight changes after upgrade of packages

--- pages/index.js
+++ pages/index.js
@@ -17,15 +17,15 @@ export default function IndexPage({ postData }) {
         <title>Jeremy Puchta</title>
       </Head>
       <section>
-        <h1 className="text-4xl font-bold mt-0 mb-2 sm:text-5xl">Hey, I'm Jerry!</h1>
-        <p className="leading-slight text-gray-800 sm:text-lg">
+        <h1 className="text-3xl font-bold mt-0 mb-2 sm:text-4xl">Hey, I'm Jerry!</h1>
+        <p className="text-gray-900 text-lg">
           I'm a software engineer in love with learning new things.
           On this website I share my latest work and thoughts about various subjects.
-          Reach out to me on <a href="https://twitter.com/jeremypuchta">Twitter</a> or meet me in person in <a href="https://www.google.com/maps/place/Leipzig/">Leipzig</a> for a coffee or beer.
+          Reach out to me on <a href="https://twitter.com/jeremypuchta" target="_blank">Twitter</a> or meet me in person in <a href="https://www.google.com/maps/place/Leipzig/" target="_blank">Leipzig</a> for a coffee or beer.
         </p>
       </section>
       <section className="my-4">
-        <h2 className="mb-4 text-3xl font-bold sm:text-4xl">Articles</h2>
+        <h2 className="mb-4 text-2xl font-bold sm:text-3xl">Articles</h2>
         {postData.map((post) => (
           <Post
             key={post.frontMatter.title}
